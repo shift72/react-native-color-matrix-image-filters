dd0ffec2cc84631f1034c3b1b84b09ab49a0d4ca
Daniel Tebbutt <daniel@shift72.com>
2020-05-26 20:45:54 +1200

    tvOS target

---
+++ ios/RNColorMatrixImageFilters-tvos/RNColorMatrixImageFilters_tvos.h
@@ -0,0 +1,13 @@
+//
+//  RNColorMatrixImageFilters_tvos.h
+//  RNColorMatrixImageFilters-tvos
+//
+//  Created by Daniel Tebbutt on 26/05/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNColorMatrixImageFilters_tvos : NSObject
+
+@end
---
+++ ios/RNColorMatrixImageFilters-tvos/RNColorMatrixImageFilters_tvos.m
@@ -0,0 +1,13 @@
+//
+//  RNColorMatrixImageFilters_tvos.m
+//  RNColorMatrixImageFilters-tvos
+//
+//  Created by Daniel Tebbutt on 26/05/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import "RNColorMatrixImageFilters_tvos.h"
+
+@implementation RNColorMatrixImageFilters_tvos
+
+@end
